7ec1b2a2fd3bc6fd9c0491431cab263588ade3e4
Zennie <zbaig@fau.edu>
2021-01-15 20:26:27 -0500

    Just the basic scaffolding of a C# console application in .NET Core.

---
+++ Tech-Journal-ConsoleApp/Tech-Journal-ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tech_Journal_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
